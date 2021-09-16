211d8d9344f1299da70ccaa5d6ef06d39cd79e8b
Roomsie Jones <seangravito@gmail.com>
2021-09-15 22:22:22 -0500

    added a game loop timer to handle re-rendering of the game

--- src/main/java/com/renaud/Game.java
+++ src/main/java/com/renaud/Game.java
@@ -2,22 +2,37 @@ package com.renaud;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Game extends JPanel {
+// JPanel is what allows it into the JFrame, ActionListener is what allows us to create the loop using Timer
+public class Game extends JPanel implements ActionListener {
     // by using this, we give Game a serial version
     private static final long serialVersionUID = 1L;
+
+    Timer gameLoopTimer;
 
     public Game(){
         // allows user to move in the game without having to click on the game
         setFocusable(true);
+        // runs through this code ever "delay" in milliseconds
+        gameLoopTimer = new Timer(1000, this);
+        gameLoopTimer.start();
     }
+
+
 
     public void paint(Graphics g){
         // we will be painting with the variable g using the super class
         super.paint(g);
         // using a caste system converting g variable from a Graphics variable into a Graphics 2D variable
         Graphics2D g2d = (Graphics2D) g;
+    }
 
-        
+    // needed to get rid of the error from the implementation of the ActionListener
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        // an in-built method with paint that re-calls the paint class and this runs with the gameloop timer currently
+        repaint();
     }
 }
