377bdd59360fc386877d9f2e8383673ceccc74ed
Roomsie Jones <seangravito@gmail.com>
2021-09-15 21:55:28 -0500

    added main app and created a JFrame for the game to play within

---
+++ src/main/java/com/renaud/Main.java
@@ -0,0 +1,19 @@
+package com.renaud;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.pack();
+        frame.setSize(640, 480);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+
+    }
+}
